03dd4f2c996c99ec7a14595a20b21b94cdff08fd
thienphonqanh <thienphonqanh@gmail.com>
2024-12-03 13:36:35 +0700

    a

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
 import { Params } from 'express-serve-static-core'
 import { USER_MESSAGES } from '~/constants/messages'
-import { Users } from '~/models/entity/users'
 import { UpdateMeReqBody } from '~/requests/user.requests'
+import { GetMeResponse } from '~/responses/user.responses'
 import userServices from '~/services/user.services'
 import { ResponseDataType } from '~/util/responseDataType'
 
-export const getMeController = async (req: Request, res: Response<ResponseDataType<Users>>, next: NextFunction) => {
+export const getMeController = async (
+  req: Request,
+  res: Response<ResponseDataType<GetMeResponse>>,
+  next: NextFunction
+) => {
   const { user_id } = req.decoded_authorization
-  const result: Users = (await userServices.getMeService(user_id)) as Users
+  const result = (await userServices.getMeService(user_id)) as GetMeResponse
   return res.json({
     message: USER_MESSAGES.GET_ME_SUCCESS,
     data: result
